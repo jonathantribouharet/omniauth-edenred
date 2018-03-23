2452ee628f84f7bc24ac3e8b947849fe27f2cf75
Jonathan Vukovich Tribouharet <jonathan@eivo.fr>
2018-03-23 13:32:31 +0100

    Improve JWT decode error

--- lib/omniauth/strategies/edenred.rb
+++ lib/omniauth/strategies/edenred.rb
@@ -26,6 +26,33 @@ module OmniAuth
 				end
 			end
 
+			# Added JWT.decode
+			def callback_phase
+				begin
+					error = request.params["error_reason"] || request.params["error"]
+					if error
+						fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+					elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
+						fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+					else
+						self.access_token = build_access_token
+						self.access_token = access_token.refresh! if access_token.expired?
+
+						@raw_info = JWT.decode(access_token.params['id_token'], nil, false).first
+						env['omniauth.auth'] = auth_hash
+						call_app!
+					end
+				rescue ::OAuth2::Error, CallbackError => e
+					fail!(:invalid_credentials, e)
+				rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
+					fail!(:timeout, e)
+				rescue ::SocketError => e
+					fail!(:failed_to_connect, e)
+				rescue ::JWT::DecodeError => e
+					fail!(:jwt_decode, e)
+				end
+			end
+
 			uid { raw_info['username'] }
 
 			info do
@@ -42,7 +69,7 @@ module OmniAuth
 			end
 
 			def raw_info
-				@raw_info ||= JWT.decode(access_token.params['id_token'], nil, false).first
+				@raw_info || {}
 			end
 
 			# Required for omniauth-oauth2 >= 1.4
